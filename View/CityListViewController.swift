94d337123a973b39607fb59ad271dfc4a8465051
Paulo Eduardo Correa <paulo.ec@hotmail.com>
2019-02-28 13:34:10 +0700

    Project Organization
    * Added folders for View/ViewModel/Model
    * Added folder for resources

---
+++ View/CityListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Backbase
+//
+//  Created by Paulo Correa on 28/02/19.
+//  Copyright © 2019 Paulo Correa. All rights reserved.
+//
+
+import UIKit
+
+class CityListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Cities"
+        view.backgroundColor = .red
+    }
+    
+}
+
