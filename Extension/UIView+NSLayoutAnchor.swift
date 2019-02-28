6fd65ffc9f3aecdbdf17d470440ad4ef6c0abb20
Paulo Eduardo Correa <paulo.ec@hotmail.com>
2019-02-28 20:27:14 +0700

    Adding UI Elements
    * Added UITableView and MKMapView
    * Support portrait and Landscape
    * Remove Main Storyboard to support only code
    * Added extension to support work with constraints

---
+++ Extension/UIView+NSLayoutAnchor.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+NSLayoutAnchor.swift
+//  Backbase
+//
+//  Created by Paulo Correa on 28/02/19.
+//  Copyright © 2019 Paulo Correa. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor, padding: UIEdgeInsets = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+    }
+}
