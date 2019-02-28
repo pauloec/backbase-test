ffe85cc149e3eb09ff1c143b455687c1096e3b35
Paulo Eduardo Correa <paulo.ec@hotmail.com>
2019-02-28 21:19:49 +0700

    Map Optimization
    * MapView set as lazy var to avoid memory usage if only portrait

--- Extension/UIView+NSLayoutAnchor.swift
+++ Extension/UIView+NSLayoutAnchor.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor, padding: UIEdgeInsets = .zero) {
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
         
-        topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
-        leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
-        bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
-        trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width)
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height)
+        }
+    }
+    
+    func anchorSuperview() {
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
     }
 }
--- View/CityListViewController.swift
+++ View/CityListViewController.swift
@@ -7,15 +7,90 @@
 //
 
 import UIKit
+import MapKit
 
 class CityListViewController: UIViewController {
+    
+    private var tableView: UITableView = UITableView()
+    private lazy var mapView: MKMapView = {
+        return MKMapView()
+    }()
+    
+    let cellReuseIdentifier = "cellCity"
+    
+    fileprivate var tableViewPortraitConstraint: NSLayoutConstraint!
+    fileprivate var tableViewLandscapeConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "Cities"
-        view.backgroundColor = .red
+        title = "Cities"
+        view.backgroundColor = .white
+        
+        setupTableView()
+        setupLayout()
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        if #available(iOS 11.0, *) {
+            tableViewPortraitConstraint = tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            tableView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.bottomAnchor, trailing: nil)
+        } else {
+            tableViewPortraitConstraint = tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            tableView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: nil)
+        }
+        
+        tableViewLandscapeConstraint = tableView.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -55)
+        setupOrientation()
+    }
+    
+    private func setupMapView() {
+        view.insertSubview(mapView, belowSubview: tableView)
+        mapView.anchor(top: view.topAnchor, leading: view.centerXAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: UIEdgeInsets(top: 0, left: -50, bottom: 0, right: 0))
+    }
+    
+    private func setupOrientation() {
+        if UIDevice.current.orientation.isPortrait {
+            NSLayoutConstraint.deactivate([tableViewLandscapeConstraint])
+            NSLayoutConstraint.activate([tableViewPortraitConstraint])
+        } else {
+            if !(mapView.isDescendant(of: view)) {
+                setupMapView()
+            }
+            
+            NSLayoutConstraint.deactivate([tableViewPortraitConstraint])
+            NSLayoutConstraint.activate([tableViewLandscapeConstraint])
+        }
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        setupOrientation()
     }
     
 }
+
+extension CityListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellReuseIdentifier)
+        }
+        
+        cell!.textLabel?.text = "City"
+        cell!.detailTextLabel?.text = "Detail"
+        return cell!
+    }
+}
+
+
 
