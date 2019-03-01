d1349cfee6365e7c8e47746489a02a2e6c4e9df7
Paulo Eduardo Correa <paulo.ec@hotmail.com>
2019-03-02 03:49:45 +0700

    Adding folders

---
+++ Backbase/Extension/UIView+NSLayoutAnchor.swift
@@ -0,0 +1,43 @@
+//
+//  UIView+NSLayoutAnchor.swift
+//  Backbase
+//
+//  Created by Paulo Correa on 28/02/19.
+//  Copyright © 2019 Paulo Correa. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width)
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height)
+        }
+    }
+    
+    func anchorSuperview() {
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
+    }
+}
